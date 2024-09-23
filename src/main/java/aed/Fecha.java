88ca8d377febcee2e055943db03e90aaac8b3e97
Agustin <agustinorco@gmail.com>
2024-09-23 16:52:23 -0300

    correccion

--- src/main/java/aed/Fecha.java
+++ src/main/java/aed/Fecha.java
@@ -1,6 +1,7 @@
 package aed;
 
 public class Fecha {
+
     private int dia;
     private int mes;
 
@@ -29,31 +30,31 @@ public class Fecha {
 
     @Override
     public boolean equals(Object otra) {
-        Fecha otraFecha = Fecha (otra);
-        if(get.class(fecha)==get.class(otraFecha)){
-            return this.dia==otraFecha.dia && this.mes==otraFecha.mes;
-        } else{
+        Fecha otraFecha = (Fecha) otra;
+        if (this.getClass() == otraFecha.getClass()) {
+            return this.dia == otraFecha.dia && this.mes == otraFecha.mes;
+        } else {
             return false;
         }
     }
 
     public void incrementarDia() {
-        if(this.dia==diasEnMes(this.mes)){
-            if(this.mes==12){
-                this.mes=1;
+        if (this.dia == diasEnMes(this.mes)) {
+            if (this.mes == 12) {
+                this.mes = 1;
             }
-            this.dia=1
-        } else{
+            this.dia = 1;
+        } else {
             this.dia++;
         }
     }
 
     private int diasEnMes(int mes) {
         int dias[] = {
-                // ene, feb, mar, abr, may, jun
-                31, 28, 31, 30, 31, 30,
-                // jul, ago, sep, oct, nov, dic
-                31, 31, 30, 31, 30, 31
+            // ene, feb, mar, abr, may, jun
+            31, 28, 31, 30, 31, 30,
+            // jul, ago, sep, oct, nov, dic
+            31, 31, 30, 31, 30, 31
         };
         return dias[mes - 1];
     }
